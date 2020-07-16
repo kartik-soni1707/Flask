3212e66988dbc045d5485409761322627c6855f9
kartik-soni1707 <its.kartiksoni@gmail.com>
2020-07-16 23:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,21 +19,21 @@ def pred():
     # file.save(('data.jpg'))
     model = load_model('facefeatures_new_model.h5')
     from PIL import Image
-    face = Image.open("wow.jpg")
-    face = cv2.resize(np.float32(face), (224, 224))
-    im = Image.fromarray(face, 'RGB')
+    #face = Image.open("wow.jpg")
+    #face = cv2.resize(np.float32(face), (224, 224))
+    #im = Image.fromarray(face, 'RGB')
     # Resizing into 128x128 because we trained the model with this image size.
-    img_array = np.array(im)
+    #img_array = np.array(im)
     # Our keras model used a 4D tensor, (images x height x width x channel)
     # So changing dimension 128x128x3 into 1x128x128x3
-    img_array = np.expand_dims(img_array, axis=0)
-    pred = model.predict(img_array)
-    if(pred[0][1]==1):
-        name="On"
-    else:
-        name="Off"
+    #img_array = np.expand_dims(img_array, axis=0)
+    #pred = model.predict(img_array)
+    #if(pred[0][1]==1):
+    #    name="On"
+    #else:
+    #    name="Off"
 
-    return render_template('index.html', res=name)
+    return render_template('index.html', res=" ")
 if __name__=="__main__":
 
     app.run(debug=True)
